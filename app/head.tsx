d1dccd107c81567dbfb6798cde13eb3143fe1caf
Aditya Raj <rajaditya27145@gmail.com>
2023-05-04 22:44:03 +0530

    fix home meta tag

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Aditya Raj | Frontend Engineer | Work</title>
+      <title>Aditya Raj | Frontend Engineer</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="title" content="Aditya Raj | Frontend Engineer" />
       <meta
